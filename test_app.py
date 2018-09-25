9927d76e971a99a227ff578eb66e637406098132
BarnaTB <barnabastb2@gmail.com>
2018-09-25 14:57:33 +0300

    ft(Username should be different from the name.): Commit delivers a feature that enables a user to register only when they have valid username and name which are not the same and not less than 4 characters long. [Deliver #160751041]

--- test_app.py
+++ test_app.py
@@ -5,7 +5,7 @@ from app import User
 class TestUser(unittest.TestCase):
     def setUp(self):
         self.user = User(
-            'Barnabas Tumuhairwe', 'Barna', 'barna@gmail.com', 30, 'password'
+            'Barnabas Tumuhairwe', 'Barna', 'barna@gmail.com', '3', 'password'
             )
 
     def test_creation(self):
@@ -19,3 +19,14 @@ class TestUser(unittest.TestCase):
     def test_validate_password(self):
         """Test a password is valid"""
         self.assertEqual(self.user.validate_password(), False)
+
+    def test_validate_user_names(self):
+        """
+        Test that user names are not equal to each other and are not
+        less than 4 characters long.
+        """
+        self.assertEqual(self.user.validate_user_names(), True)
+
+    def test_validate_age(self):
+        """Test that a user's age is an integer greater or equal to 0."""
+        self.assertEqual(self.user.validate_age(), False)
