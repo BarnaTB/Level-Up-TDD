0877c8ac2ab5771ee920b9fc211321d88f008747
BarnaTB <barnabastb2@gmail.com>
2018-09-26 17:13:30 +0300

    ft(User should be able to register and login with their correct credentials): Commit delivers a feature that enables a user to register and login with their credentials. [Deliver #160751019 #160751118]

--- test_app.py
+++ test_app.py
@@ -6,7 +6,11 @@ class TestUser(unittest.TestCase):
     def setUp(self):
         self.user = User(
             'Barnabas Tumuhairwe', 'Barna', 'barna@gmail.com', '3', 'password'
-            )
+        )
+        self.legit_user = User(
+            'Barnabas Tumuhairwe', 'Barna', 'barna@gmail.com', 21, 'P@ss123'
+        )
+        self.registered_user = self.legit_user.register_user()
 
     def test_creation(self):
         """Test the class is created"""
@@ -30,3 +34,21 @@ class TestUser(unittest.TestCase):
     def test_validate_age(self):
         """Test that a user's age is an integer greater or equal to 0."""
         self.assertEqual(self.user.validate_age(), False)
+
+    def test_register_user(self):
+        """Test a user can register successfully"""
+        self.assertEqual(
+            self.legit_user.register_user(),
+            'User Barna has been registered successfully!'
+        )
+
+    def test_login_user(self):
+        """Test user can login successfully"""
+        self.assertEqual(
+            self.legit_user.register_user(),
+            'User Barna has been registered successfully!'
+        )
+        self.assertEqual(
+            self.legit_user.login_user('Barna', 'P@ss123'),
+            'You are logged in now'
+        )
